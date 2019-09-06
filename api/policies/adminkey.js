a8484c49f2c3b25cf1335ae537166628c0d99661
Thankalv <53212577+Thankalv@users.noreply.github.com>
2019-09-06 12:12:29 +0300

    Create adminkey.js

---
+++ api/policies/adminkey.js
@@ -0,0 +1,28 @@
+/**
+ * adminKey
+ *
+ * @module      :: Policy
+ * @description :: http-header based auth Policy to allow API requests with valid API-key
+ * @docs        :: http://sailsjs.org/#!documentation/policies
+ *
+ */
+module.exports = function(req, res, next) {
+
+    var headerToken = req.header("Admin-Key")
+    //console.log(req.headers);
+    //console.log('Remote connection ip:', req.connection.remoteAddress);
+
+    // Client is allowed, proceed to the next policy,
+    if (headerToken == sails.config.adminKey)
+    {
+        return next();
+    } 
+    else 
+    {
+      // Client is not allowed
+      res.statusCode = 401;
+      return res.json({err: "Unauthorized. Invalid 'Admin-Key' API header "});
+
+    }
+  };
+  
